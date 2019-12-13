426b8a49d29138ef4f40cd24e27ad8010e1e3aa5
Yogesh Upadhyay <53992168+YogeshUpdhyay@users.noreply.github.com>
2019-12-13 12:12:18 +0530

    Add files via upload

---
+++ speechrecog.py
@@ -0,0 +1,105 @@
+import os
+import librosa  
+import random #for audio processing
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile #for audio processing
+import warnings
+warnings.filterwarnings("ignore")
+
+train_audio_path = '/Users/Apple/Documents/speech_commands_v0.01'
+
+labels=os.listdir(train_audio_path)
+labels.remove('.DS_Store')
+labels.remove('validation_list.txt')
+labels.remove('LICENSE')
+labels.remove('README.md')
+labels.remove('testing_list.txt')
+#find count of each label and plot bar graph
+no_of_recordings=[]
+for label in labels:
+    waves = [f for f in os.listdir(train_audio_path + '/'+ label) if f.endswith('.wav')]
+    no_of_recordings.append(len(waves))
+
+target_list = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go']
+unknown_list = [d for d in labels if d not in target_list and d != '_background_noise_' ]
+
+all_wave = []
+all_label = []
+for label in target_list:
+    print(label)
+    waves = [f for f in os.listdir(train_audio_path + '/'+ label) if f.endswith('.wav')]
+    for wav in waves:
+        samples, sample_rate = librosa.load(train_audio_path + '/' + label + '/' + wav, sr = 16000)
+        samples = librosa.resample(samples, sample_rate, 8000)
+        if(len(samples)== 8000) : 
+            all_wave.append(samples)
+            all_label.append(label)
+"""
+for label in all_label:
+    encoding = [0] *len(LABEL_TO_INDEX_MAP)
+    
+def get_model(input,dropout):
+"""
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y=le.fit_transform(all_label)
+classes= list(le.classes_)
+
+from sklearn.model_selection import train_test_split
+x_tr, x_val, y_tr, y_val = train_test_split(np.array(all_wave),np.array(y),stratify=y,test_size = 0.2,random_state=777,shuffle=True)
+
+from keras.layers import Dense, Dropout, Flatten, Conv1D, Input, MaxPooling1D
+from keras.models import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+inputs = Input(shape=(8000,1))
+
+#First Conv1D layer
+conv = Conv1D(8,13, padding='valid', activation='relu', strides=1)(inputs)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Second Conv1D layer
+conv = Conv1D(16, 11, padding='valid', activation='relu', strides=1)(conv)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Third Conv1D layer
+conv = Conv1D(32, 9, padding='valid', activation='relu', strides=1)(conv)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Fourth Conv1D layer
+conv = Conv1D(64, 7, padding='valid', activation='relu', strides=1)(conv)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+
+#Flatten layer
+conv = Flatten()(conv)
+
+#Dense Layer 1
+conv = Dense(256, activation='relu')(conv)
+conv = Dropout(0.3)(conv)
+
+conv.add(Flatten())
+
+#Dense Layer 2
+conv = Dense(128, activation='relu')(conv)
+conv = Dropout(0.3)(conv)
+
+outputs = Dense(len(labels), activation='softmax')(conv)
+
+model = Model(inputs, outputs)
+model.summary()
+model.compile(optimizer = 'adam',loss = 'categorical_crossentropy',metrics=['accuracy'])
+history=model.fit(x_tr, y_tr ,epochs=100, batch_size=32, validation_data=(x_val,y_val))
+
+from matplotlib import pyplot 
+pyplot.plot(history.history['loss'], label='train') 
+pyplot.plot(history.history['val_loss'], label='test') 
+pyplot.legend() 
+pyplot.show()
+
